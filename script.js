e65ddd437894113a98191a600ef46a5de50b718e
kioksianto <brenda.sianto@gmail.com>
2021-03-04 10:17:30 +0300

    overlay description

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-//Frond-end logic
+/*//Frond-end logic
 $(document).ready(function(){
 $('ul#row-1').on('mouseenter',function(){
 var title = $(this).children().data('title');
@@ -14,6 +14,7 @@ if(desc === null){
 if(title === null){
     title = '';
 }
+//Frond-end logic
 //overlay div 
 $(this).append('<div class = "overlay"></div>')
 
@@ -25,4 +26,53 @@ overlay.html('<h3>'+title+'</h3><p>'+desc+'</p>');
 
 //fade in overlay
 overlay.fadein(800);
+});*/
+
+$(document).ready(function(){
+    $('div#whatwedo img').on('mouseenter',function(){
+        var title = $(this).children().data('title');
+        var desc = $(this).children().data('desc');
+
+        //validation
+        if(desc === null){
+            desc = 'Click To Enlarge';
+
+        }
+        if(title === null){
+            title = '';
+        }
+        //create overlay div
+        $(this).append('div class="overlay"></div>');
+
+        //get the overlay div
+        var overlay = $(this).children('.overlay');
+
+        //adding HTML to overlay
+        overlay.html('<h3>'+title+'</h3><p>'+desc+'</p>');
+
+        //fade in overlay
+        overlay.fadeIn(800);
+
+
+        
+    });
+    //Mouseleave overlay
+    $('div#whatwedo img').on('mouseleave',function(){
+       
+        //create overlay div
+        $(this).append('div class="overlay"></div>');
+
+        //get the overlay div
+        var overlay = $(this).children('.overlay');
+
+        //fade out overlay
+        overlay.fadeOut(500);
+
+
+    });
+
+
+
 });
+
+ 
