8dc243c299fd695902793e7a2c74cc1ef61ecf81
kioksianto <brenda.sianto@gmail.com>
2021-03-04 16:01:20 +0300

    contact page and mail chimp update

--- script.js
+++ script.js
@@ -1,78 +1,53 @@
-/*//Frond-end logic
-$(document).ready(function(){
-$('ul#row-1').on('mouseenter',function(){
-var title = $(this).children().data('title');
-var desc = $(this).children().data('desc');
-});
-//business-logic
-//validation
-if(desc === null){
-    desc = 'Click To Enlarge';
 
-}
-
-if(title === null){
-    title = '';
-}
-//Frond-end logic
-//overlay div 
-$(this).append('<div class = "overlay"></div>')
-
-//getting the overlay div
-var overlay = $(this).children('.overlay');
-
-//adding HTML to overlay
-overlay.html('<h3>'+title+'</h3><p>'+desc+'</p>');
-
-//fade in overlay
-overlay.fadein(800);
-});*/
+//Overlay style on What We Do section on Website
 
 $(document).ready(function(){
-    $('div#whatwedo img').on('mouseenter',function(){
-        var title = $(this).children().data('title');
-        var desc = $(this).children().data('desc');
 
-        //validation
-        if(desc === null){
-            desc = 'Click To Enlarge';
+    // Design Description
+    $('#desc-image-1').click(function(){
+        $('#desc-1').show();
+        $('#desc-image-1').hide();
 
-        }
-        if(title === null){
-            title = '';
-        }
-        //create overlay div
-        $(this).append('div class="overlay"></div>');
+    })
 
-        //get the overlay div
-        var overlay = $(this).children('.overlay');
+    $('#desc-1').click(function(){
+        $('#desc-image-1').show();
+        $('#desc-1').hide();
 
-        //adding HTML to overlay
-        overlay.html('<h3>'+title+'</h3><p>'+desc+'</p>');
+    })
 
-        //fade in overlay
-        overlay.fadeIn(800);
+    // Development Description
+    $('#desc-image-2').click(function(){
+        $('#desc-2').show();
+        $('#desc-image-2').hide();
 
+    })
 
-        
-    });
-    //Mouseleave overlay
-    $('div#whatwedo img').on('mouseleave',function(){
-       
-        //create overlay div
-        $(this).append('div class="overlay"></div>');
+    $('#desc-2').click(function(){
+        $('#desc-image-2').show();
+        $('#desc-2').hide();
 
-        //get the overlay div
-        var overlay = $(this).children('.overlay');
+    })
 
-        //fade out overlay
-        overlay.fadeOut(500);
+    // Product Design Description
+    $('#desc-image-3').click(function(){
+        $('#desc-3').show();
+        $('#desc-image-3').hide();
 
+    })
 
-    });
+    $('#desc-3').click(function(){
+        $('#desc-image-3').show();
+        $('#desc-3').hide();
 
-
+    })
 
 });
 
+
+//Message alert after subscription
+function setName(){
+    var name = document.getElementById('mce-NAME');
+    alert(`Hello ${name}, thank you for subscribing.`);
+}
  
